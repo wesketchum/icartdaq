db1857462d514182b53186d2f28c457a6fb285d3
Kurt Biery <biery@fnal.gov>
2013-04-30 08:23:25 -0500

    Moved PMT helper scripts to artdaq.

--- tools/mpi_cleaner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ps -A aww -o pid,cmd --no-header | grep "$1 -p ${@:2}" | grep -v grep | sed "s/^[ ]*//" | cut -d" " -f1 | xargs kill
-sleep 3
-ps -A aww -o pid,cmd --no-header | grep "$1 -p ${@:2}" | grep -v grep | sed "s/^[ ]*//" | cut -d" " -f1 | xargs kill -9
--- tools/mpi_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hostname=`hostname`
-
-echo "STARTING:$hostname:$1:${@:2}"
-$1 -p ${@:2} #&> $1.$2
-echo "EXITING:$hostname:$?:$1:${@:2}"
