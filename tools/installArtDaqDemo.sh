2712fb8bf4af44ccb15443abac2c2967ca048192
Kurt Biery <biery@fnal.gov>
2013-05-06 11:55:27 -0500

    make use of Lynn's downloadArtDaq script during installation

--- tools/installArtDaqDemo.sh
+++ tools/installArtDaqDemo.sh
@@ -7,34 +7,16 @@ if [[ "$response" != "y" ]]; then
     exit
 fi
 
+mkdir products
 mkdir download
 cd download
-wget -nv -N http://oink.fnal.gov/distro/private-products-setup/upsfiles-dir.tar.gz
-wget -nv -N http://oink.fnal.gov/distro/relocatable-ups/ups-upd-4.9.7-slf6-x86_64.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/art/art_externals-0.06.04-slf6-x86_64-e2-prof.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/art/art_suite-1.07.01-slf6-x86_64-e2-prof.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/packages/artdaq-1.00.00-slf6-x86_64-e2-eth-prof.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/packages/mpich-3.0.2-slf6-x86_64-prof.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/packages/xmlrpc_c-1.25.18-slf6-x86_64-e2-prof.tar.bz2
-wget -nv -N http://oink.fnal.gov/distro/packages/gccxml-0.9.20130503-slf6-x86_64-gcc47.tar.bz2
-
-cd ..
-mkdir products
-cd products
-tar -xvf ../download/upsfiles-dir.tar.gz
-tar -xvf ../download/ups-upd-4.9.7-slf6-x86_64.tar.bz2
-tar -xvf ../download/art_externals-0.06.04-slf6-x86_64-e2-prof.tar.bz2
-tar -xvf ../download/art_suite-1.07.01-slf6-x86_64-e2-prof.tar.bz2
-tar -xvf ../download/artdaq-1.00.00-slf6-x86_64-e2-eth-prof.tar.bz2
-tar -xvf ../download/mpich-3.0.2-slf6-x86_64-prof.tar.bz2
-tar -xvf ../download/xmlrpc_c-1.25.18-slf6-x86_64-e2-prof.tar.bz2
-tar -xvf ../download/gccxml-0.9.20130503-slf6-x86_64-gcc47.tar.bz2
+../downloadArtDaq-v1_07_01.sh ../products e2:eth prof
 
 cd ..
 mkdir demoBuild
 
 if [[ ! -e artdaq-demo ]]; then
-    git clone ssh://p-artdaq-demo@cdcvs.fnal.gov/cvs/projects/artdaq-demo
+    git clone http://cdcvs.fnal.gov/projects/artdaq-demo
 fi
 
 if [[ ! -e ./setupARTDAQDEMO ]]; then
