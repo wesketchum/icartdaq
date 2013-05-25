40472ba1a38f683809a4972f080b4b497a8861b9
Kurt Biery <biery@fnal.gov>
2013-05-25 14:04:48 -0500

    add test for bash shell

--- tools/installArtDaqDemo.sh
+++ tools/installArtDaqDemo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this is only here in case this script is sourced instead of run in its own shell
+sh -c "[ `ps $$ | grep bash | wc -l` -gt 0 ] || { echo 'Please switch to the bash shell before installing the artdaq-demo.'; exit 1; }" || exit
+
 echo "Are you sure you want to install the artdaq demo in `pwd`? [y/n]"
 read response
 if [[ "$response" != "y" ]]; then
@@ -7,30 +10,43 @@ if [[ "$response" != "y" ]]; then
     exit
 fi
 
-mkdir products
-mkdir download
+mkdir -p products
+mkdir -p download
 cd download
 ../downloadArtDaq-v1_07_01.sh ../products e2:eth prof
 
 cd ..
-mkdir demoBuild
+mkdir -p demoBuild
 
 if [[ ! -e artdaq-demo ]]; then
     git clone http://cdcvs.fnal.gov/projects/artdaq-demo
 fi
 
-if [[ ! -e ./setupARTDAQDEMO ]]; then
-    echo "export FHICL_FILE_PATH=." > setupARTDAQDEMO
-    echo "export CETPKG_INSTALL=`pwd`/products" >> setupARTDAQDEMO
-    echo "export CETPKG_J=16" >> setupARTDAQDEMO
-    echo "#export ARTDAQDEMO_BASE_PORT=52200" >> setupARTDAQDEMO
-    echo "export DAQ_INDATA_PATH=`pwd`/artdaq-demo/test/Generators:`pwd`/artdaq-demo/inputData" >> setupARTDAQDEMO
-    echo "" >> setupARTDAQDEMO
-    echo "export ARTDAQDEMO_BUILD="`pwd`/demoBuild"" >> setupARTDAQDEMO
-    echo "export ARTDAQDEMO_REPO="`pwd`/artdaq-demo"" >> setupARTDAQDEMO
-    echo "cd \$ARTDAQDEMO_BUILD; source \$ARTDAQDEMO_REPO/ups/setup_for_development -p e2 eth" >> setupARTDAQDEMO
-    echo "" >> setupARTDAQDEMO
-    echo "alias rawEventDump=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/rawEventDump.fcl\"" >> setupARTDAQDEMO
-    echo "alias compressedEventDump=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/compressedEventDump.fcl\"" >> setupARTDAQDEMO
-    echo "alias compressedEventComparison=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/compressedEventComparison.fcl\"" >> setupARTDAQDEMO
+if [[ -e ./setupARTDAQDEMO ]]; then
+    echo "Do you want to over-write the existing setupARTDAQDEMO script? [y/n]"
+    read response
+    if [[ "$response" != "y" ]]; then
+        echo "Aborting..."
+        exit
+    fi
 fi
+
+echo "# This script is intended to be sourced." > setupARTDAQDEMO
+echo "" >> setupARTDAQDEMO
+echo "sh -c \"[ \`ps \$\$ | grep bash | wc -l\` -gt 0 ] || { echo 'Please switch to the bash shell before running the artdaq-demo.'; exit \" || exit" >> setupARTDAQDEMO
+echo "" >> setupARTDAQDEMO
+echo "source `pwd`/products/setup" > setupARTDAQDEMO
+echo "" >> setupARTDAQDEMO
+echo "export FHICL_FILE_PATH=." >> setupARTDAQDEMO
+echo "export CETPKG_INSTALL=`pwd`/products" >> setupARTDAQDEMO
+echo "export CETPKG_J=16" >> setupARTDAQDEMO
+echo "#export ARTDAQDEMO_BASE_PORT=52200" >> setupARTDAQDEMO
+echo "export DAQ_INDATA_PATH=`pwd`/artdaq-demo/test/Generators:`pwd`/artdaq-demo/inputData" >> setupARTDAQDEMO
+echo "" >> setupARTDAQDEMO
+echo "export ARTDAQDEMO_BUILD="`pwd`/demoBuild"" >> setupARTDAQDEMO
+echo "export ARTDAQDEMO_REPO="`pwd`/artdaq-demo"" >> setupARTDAQDEMO
+echo "cd \$ARTDAQDEMO_BUILD; source \$ARTDAQDEMO_REPO/ups/setup_for_development -p e2 eth" >> setupARTDAQDEMO
+echo "" >> setupARTDAQDEMO
+echo "alias rawEventDump=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/rawEventDump.fcl\"" >> setupARTDAQDEMO
+echo "alias compressedEventDump=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/compressedEventDump.fcl\"" >> setupARTDAQDEMO
+echo "alias compressedEventComparison=\"art -c `pwd`/artdaq-demo/artdaq-demo/ArtModules/fcl/compressedEventComparison.fcl\"" >> setupARTDAQDEMO
