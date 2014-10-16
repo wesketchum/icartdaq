e8da65cbb6579efeda3003938f3dcf1fdcd9a2ab
Kurt Biery <biery@fnal.gov>
2014-10-16 13:53:26 -0500

    Switched to using a pullProducts script to download the art suite.

--- tools/downloadDeps.sh
+++ tools/downloadDeps.sh
@@ -21,37 +21,15 @@ starttime=`date`
 cd ${productdir}
 
 prods="\
-artdaq_core v1_04_05 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal3}:${build_type}
-art v1_12_01 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-boost v1_56_0 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-cetlib v1_07_03 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-cetpkgsupport v1_07_00 -f NULL -z ${productdir} -g current
-clhep v2_2_0_3 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-cmake v3_0_1 -f Linux64bit+2.6-2.12 -z ${productdir}
-cpp0x v1_04_08 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-cppunit v1_12_1a -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-fftw v3_3_4 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${build_type}
-fhiclcpp v3_01_02 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-gcc v4_9_1 -f Linux64bit+2.6-2.12 -z ${productdir}
-gccxml v0_9_20140718 -f Linux64bit+2.6-2.12 -z ${productdir}
-libxml2 v2_9_1a -f Linux64bit+2.6-2.12 -z ${productdir} -q ${build_type}
-messagefacility v1_11_15 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
 mpich v3_1_2 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-python v2_7_8 -f Linux64bit+2.6-2.12 -z ${productdir}
-root v5_34_21a -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-sqlite v3_08_05_00 -f Linux64bit+2.6-2.12 -z ${productdir}
-tbb v4_2_5 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-xmlrpc_c v1_25_30 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}
-xrootd v3_3_4b -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}"
+xmlrpc_c v1_25_30 -f Linux64bit+2.6-2.12 -z ${productdir} -q ${basequal2}:${build_type}"
 
 # Some tarfiles have names that deviate from the standard "template",
 # so we can't use the download function's algorithm
 
 prods2="\
-cetbuildtools/v4_02_02/cetbuildtools-4.02.02-noarch.tar.bz2
 smc_compiler/v6_1_0/smc_compiler-6.1.0-noarch.tar.bz2
-TRACE/v3_03_03/TRACE-3.03.03-slf6.tar.bz2
-ups/v5_1_2/ups-5.1.2-Linux64bit%2B2.6-2.12.tar.bz2"
+TRACE/v3_03_03/TRACE-3.03.03-slf6.tar.bz2"
 
 # $1=prod_area $2="prod_lines"
 
@@ -79,6 +57,24 @@ download()
 }
 
 cd ${productdir}
+
+# 16-Oct-2014, KAB: switched to using pullProducts for basic art suite
+export savedPRODUCTS=${PRODUCTS}
+installDir=`pwd`
+simpleQual=`echo ${basequal} | sed 's/:eth//g' | sed 's/eth://g' | sed 's/ib://g' | sed 's/:ib//g'`
+artVersion=v1_12_01
+pullScript=pullProducts-${artVersion}
+url=http://scisoft.fnal.gov/scisoft/projects/art/${artVersion}/${pullScript}
+echo url=$url
+wget $url 2>/dev/null
+chmod +x ${pullScript}
+mkdir tarfiles
+cd tarfiles
+export PRODUCTS=${installDir}
+../${pullScript} ${installDir} slf6 artdev ${simpleQual} ${build_type}
+export PRODUCTS=${savedPRODUCTS}
+cd ${installDir}
+rm -rf tarfiles
 
 for packagestring in `echo $prods2 | tr " " "\n"`; do
     url=http://scisoft.fnal.gov/scisoft/packages/$packagestring
