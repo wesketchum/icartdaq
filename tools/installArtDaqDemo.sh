4ea7b031914fc81465b56633af1a06625b86e755
John Freeman <jcfree@fnal.gov>
2015-03-05 11:26:38 -0600

    Bump version to v2_03_00; now relies on artdaq v1_12_08 and artdaq_core_demo v1_02_00

--- tools/installArtDaqDemo.sh
+++ tools/installArtDaqDemo.sh
@@ -95,19 +95,19 @@ function install_package {
 if [ -n "${opt_HEAD-}" ];then
 install_package artdaq-core develop
 else
-install_package artdaq-core v1_04_09 e6 s6
+install_package artdaq-core v1_04_10 e6 s6
 fi
 
 if [ -n "${opt_HEAD-}" ];then
 install_package artdaq-core-demo develop
 else
-install_package artdaq-core-demo v1_01_00
+install_package artdaq-core-demo v1_02_00
 fi
 
 if [ -n "${opt_HEAD-}" ];then
 install_package artdaq develop
 else
-install_package artdaq v1_12_06 e6 s6 eth
+install_package artdaq v1_12_08 e6 s6 eth
 fi
 
 if [  -n "${opt_HEAD-}" ];then
